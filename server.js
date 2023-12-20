9a403e16ae266aa33671f91cbe776221435b73d0
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-12-20 09:44:20 +0200

    update server.js file

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 import express from 'express'
 import Config from './src/config/environment.js'
-import serverErrorHandler from './src/utils/error/serverErrorHandler.js'
-import clientErrorHandler from './src/utils/error/clientErrorHandler.js'
+import ErrorHandler from './src/utils/error/ErrorHandler.js'
+
+import apiRouter from './src/routers/index.js'
+import { NotFound } from './src/utils/error/NotFoundError.js'
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// use api router
+app.use('/api/v1/', apiRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// client error handler
-app.use(clientErrorHandler)
+// not found
+app.all('*', NotFound)
 
 // error handler
-app.use(serverErrorHandler)
+app.use(ErrorHandler)
 
 app.listen(Config.server.port, () => {
   console.log(`listening on port ${Config.server.port}`)
