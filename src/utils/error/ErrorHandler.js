dad72bd46eaf98ebaa27b8cbceedaca668d77151
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-12-20 09:32:31 +0200

    update Error handler

--- src/utils/error/ErrorHandler.js
+++ src/utils/error/ErrorHandler.js
@@ -1,17 +1,18 @@
 import CustomError from './CustomError.js'
 
 const ErrorHandler = (error, req, res, next) => {
-  console.log({ error })
+  console.log(error)
   // Custom error
   if (error instanceof CustomError) {
-    res.status(error.statusCode).json({
+    return res.status(error.statusCode).json({
       success: false,
       message: error.message,
       status_code: error.statusCode,
     })
   }
+
   // Server error
-  res.status(500).json({
+  return res.status(500).json({
     success: false,
     message: 'Something went wrong!',
     status_code: res.statusCode,
