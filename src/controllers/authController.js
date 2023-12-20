393abee1d4454706ce36442965baa7894681e06f
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-12-20 09:24:42 +0200

    create auth controller

---
+++ src/controllers/authController.js
@@ -0,0 +1,13 @@
+/**
+ * login
+ * POST
+ * email, password
+ */
+export const login = async (req, res, next) => {}
+
+/**
+ * sign up
+ * POST
+ * first_name, last_name, email, password
+ */
+export const signUp = async (req, res, next) => {}
