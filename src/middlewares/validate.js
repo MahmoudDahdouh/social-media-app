1da3288578c9c55d4b3ecf438dcedf36984aa81b
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-12-21 09:05:14 +0200

    add asyncHandler to validate middleware

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,20 +1,22 @@
+import { asyncHandler } from '../utils/asyncHandler.js'
 import CustomError from '../utils/error/CustomError.js'
 
-export const validate = (schema) => async (req, res, next) => {
-  try {
-    const data = await schema.validate({
-      body: req.body,
-      query: req.query,
-      params: req.params,
-    })
+export const validate = (schema) =>
+  asyncHandler(async (req, res, next) => {
+    try {
+      const data = await schema.validate({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      })
 
-    // add the validated data
-    req.body = { ...data.body }
-    req.query = { ...data.query }
-    req.params = { ...data.params }
+      // add the validated data
+      req.body = { ...data.body }
+      req.query = { ...data.query }
+      req.params = { ...data.params }
 
-    return next()
-  } catch (error) {
-    return next(new CustomError(400, error.errors[0]))
-  }
-}
+      return next()
+    } catch (error) {
+      return next(new CustomError(400, error.errors[0]))
+    }
+  })
