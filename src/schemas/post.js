50662f8bf752c4c03a54a8c8064ab6e2c126eb77
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-12-23 09:53:59 +0200

    update post schema

--- src/schemas/post.js
+++ src/schemas/post.js
@@ -5,3 +5,14 @@ export const createPostSchema = Yup.object({
     content: Yup.string().label('Content').trim().max(5000).required(),
   }),
 })
+export const updatePostSchema = Yup.object({
+  body: Yup.object({
+    content: Yup.string().label('Content').trim().max(5000).required(),
+  }),
+  params: Yup.object({
+    id: Yup.number()
+      .label('Post id')
+      .integer('Post id must be an integer number!')
+      .required(),
+  }),
+})
