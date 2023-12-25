6c6d009cff32e01ffb2b9f26b084886586133c2d
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-12-25 13:22:19 +0200

    find the post before update of delete

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,3 +1,4 @@
+import { sequelize } from '../db/config/connect.js'
 import Post from '../db/models/Post.js'
 import User from '../db/models/User.js'
 import StatusResponse from '../utils/StatusResponse.js'
@@ -6,7 +7,7 @@ import CustomError from '../utils/error/CustomError.js'
 /**
  * create new Post
  * POST
- * content
+ * @body content
  */
 export const createPost = async (req, res) => {
   const { content, user } = req.body
@@ -21,64 +22,98 @@ export const createPost = async (req, res) => {
 /**
  * update Post
  * PATCH
- * content
+ * @body content
+ * @param id
  */
 export const updatePost = async (req, res) => {
   const { content, user } = req.body
   const { id } = req.params
-
-  const post = await Post.update(
-    {
-      content,
-    },
-    {
+  let post = null
+  let updatedPost = null
+  await sequelize.transaction(async (t) => {
+    // find the post
+    post = await Post.findOne({
       where: {
         id,
         user_id: user.id,
       },
-    }
-  )
-  if (post[0] === 0) {
-    return res.status(403).json({
-      ...StatusResponse(403, 'Your are not allowed to edit this post!', false),
+      transaction: t,
     })
-  }
-  return res
-    .status(200)
-    .json({ ...StatusResponse(200, 'The post updated successfully!') })
+    if (!post) {
+      throw new CustomError(403, 'Your are not allowed to delete this post!')
+    }
+    if (post.dataValues.is_deleted) {
+      throw new CustomError(404, 'Post is not found!')
+    }
+
+    // update the post
+    updatedPost = await Post.update(
+      {
+        content,
+      },
+      {
+        where: {
+          id,
+          user_id: user.id,
+        },
+        transaction: t,
+      }
+    )
+  })
+
+  post.dataValues.content = content
+  post.dataValues.updated_at = new Date()
+
+  return res.status(200).json({
+    ...StatusResponse(200, 'The post updated successfully!'),
+    post,
+  })
 }
 
 /**
  * delete Post
  * DELETE
+ * @body id
  */
 export const deletePost = async (req, res) => {
   const { id, user } = req.body
-
-  const post = await Post.update(
-    {
-      is_deleted: true,
-    },
-    {
+  let updatedResult = null
+  let post = null
+  await sequelize.transaction(async (t) => {
+    // find the post
+    post = await Post.findOne({
       where: {
         id,
         user_id: user.id,
       },
-    }
-  )
-
-  if (post[0] === 0) {
-    return res.status(403).json({
-      ...StatusResponse(
-        403,
-        'Your are not allowed to delete this post!',
-        false
-      ),
+      transaction: t,
     })
-  }
-  return res
+    if (!post) {
+      throw new CustomError(403, 'Your are not allowed to delete this post!')
+    }
+    if (post.dataValues.is_deleted) {
+      throw new CustomError(409, 'Post is already deleted!')
+    }
+    // update post
+    updatedResult = await Post.update(
+      {
+        is_deleted: true,
+      },
+      {
+        where: {
+          id,
+          user_id: user.id,
+        },
+        transaction: t,
+      }
+    )
+  })
+  post.dataValues.is_deleted = true
+  post.dataValues.updated_at = new Date()
+
+  res
     .status(200)
-    .json({ ...StatusResponse(200, 'The post deleted successfully!') })
+    .json({ ...StatusResponse(200, 'The post deleted successfully!'), post })
 }
 
 /**
@@ -95,7 +130,7 @@ export const getPost = async (req, res) => {
       model: User,
       as: 'user',
       attributes: {
-        exclude: ['password_hash', 'refresh_token', 'user_role'],
+        exclude: ['password_hash', 'refresh_token', 'user_role', 'email'],
       },
     },
   })
