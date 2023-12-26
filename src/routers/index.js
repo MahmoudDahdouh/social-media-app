ca9725ae4c43d8ffec56a9cae3a5af2cb4145202
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-12-26 10:00:08 +0200

    create friend request router

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import authRouter from './authRouter.js'
 import postsRouter from './postsRouter.js'
+import friendRequestRouter from './friendRequestRouter.js'
 
 const router = Router()
 
 router.use('/auth', authRouter)
 router.use('/posts', postsRouter)
+router.use('/friend-request', friendRequestRouter)
 
 export default router
