d879a7b0f8376b2f6bf980671e3324fcfe192336
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-12-30 12:35:21 +0200

    update sync script

--- src/db/config/sync.js
+++ src/db/config/sync.js
@@ -1,21 +1,18 @@
 import Block from '../models/Block.js'
 import Comment from '../models/Comment.js'
-import FriendRequest from '../models/FriendRequest.js'
 import Like from '../models/Like.js'
 import Post from '../models/Post.js'
 import User from '../models/User.js'
-import UserFriend from '../models/UserFriend.js'
 import UserProfile from '../models/UserProfile.js'
-
+import Follower from '../models/Follower.js'
 const sync = async () => {
   await User.sync({ alter: true })
   await UserProfile.sync({ alter: true })
   await Post.sync({ alter: true })
   await Like.sync({ alter: true })
   await Comment.sync({ alter: true })
-  await UserFriend.sync({ alter: true })
+  await Follower.sync({ alter: true })
   await Block.sync({ alter: true })
-  await FriendRequest.sync({ alter: true })
   console.log('Database synced successfully !')
 }
 sync()
