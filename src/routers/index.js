f28ee125c54641938bb23550b2c70d16294dfe53
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-12-31 16:13:21 +0200

    add followerRouter

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,12 +1,12 @@
 import { Router } from 'express'
 import authRouter from './authRouter.js'
 import postsRouter from './postsRouter.js'
-import friendRequestRouter from './friendRequestRouter.js'
+import followerRouter from './followerRouter.js'
 
 const router = Router()
 
 router.use('/auth', authRouter)
 router.use('/posts', postsRouter)
-router.use('/friend-request', friendRequestRouter)
+router.use('/', followerRouter)
 
 export default router
