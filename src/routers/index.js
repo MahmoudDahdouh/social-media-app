90d11475371633963e1923242b60d76b3061110d
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2024-01-01 16:04:53 +0200

    comments controller and router

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import authRouter from './authRouter.js'
 import postsRouter from './postsRouter.js'
 import followerRouter from './followerRouter.js'
+import commentsRouter from './commentRouter.js'
 
 const router = Router()
 
 router.use('/auth', authRouter)
 router.use('/posts', postsRouter)
 router.use('/user', followerRouter)
+router.use('/comments', commentsRouter)
 
 export default router
