c45e503ed60685e7ea8f5d238fd6514f49a32cbb
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2024-01-06 10:16:02 +0200

    unliked a post

--- src/controllers/likesController.js
+++ src/controllers/likesController.js
@@ -1,5 +1,6 @@
 import Like from '../db/models/Like.js'
 import CustomError from '../utils/error/CustomError.js'
+import StatusResponse from '../utils/StatusResponse.js'
 
 export const likePost = async (req, res) => {
   const { post_id } = req.params
@@ -12,11 +13,29 @@ export const likePost = async (req, res) => {
     })
   } catch (error) {
     if (error.original.code === 'ER_DUP_ENTRY') {
-      return res.send('you already liked the post')
+      throw new CustomError(409, 'You already liked the post!')
     } else {
       throw new CustomError(500, 'Something went wrong!')
     }
   }
 
-  res.json({ like })
+  res.json({ ...StatusResponse(201, 'You liked a post successfully!') })
+}
+
+export const unlikePost = async (req, res) => {
+  const { post_id } = req.params
+  const { user } = req.body
+
+  const like = await Like.destroy({
+    where: {
+      post_id,
+      user_id: user.id,
+    },
+  })
+
+  if (!like) {
+    throw new CustomError(404, 'Like is not found!')
+  }
+
+  res.json({ ...StatusResponse(200, 'You unliked a post successfully!') })
 }
--- src/routers/likesRouter.js
+++ src/routers/likesRouter.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { likePost } from '../controllers/likesController.js'
+import { likePost, unlikePost } from '../controllers/likesController.js'
 import { asyncHandler } from '../utils/asyncHandler.js'
 import { checkToken } from '../middlewares/auth.js'
 
 const router = Router()
 
 router.post('/:post_id', [checkToken], asyncHandler(likePost))
+router.delete('/:post_id', [checkToken], asyncHandler(unlikePost))
 
 export default router
